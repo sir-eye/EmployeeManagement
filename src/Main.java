f95132de56fc5bc460a71bf1cf7e24f34a047361
sir-eye <ial-reserve@live.com>
2024-11-26 14:25:44 -0700

    Final draft.

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,43 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
+        //Task 1: Employee objects using parameterized constructor.
         Employee emp1 = new Employee("Kavitha","Mynala","CFO",150.15);
         Employee emp2 = new Employee("Larena","Hanna","RND",250.99);
         Employee emp3 = new Employee("Touyer","Lee","Computer Analyst",59.75);
         Employee emp4 = new Employee("Jordan","Nkina","Software Engineer",95.76);
         Employee emp5 = new Employee("Daniel", "Noa", "COO", 150.23);
 
+        System.out.println("\n******************************************************************");
+        System.out.println("*****************Employee Management Project**********************");
+        System.out.println("******************************************************************\n");
+        System.out.println("\nTask 2: Calculate and display weekly salaries\n");
         System.out.println("Weekly salary of " + emp1.getTitle() + " " + emp1.getFirstName() + " is $" + emp1.calculateWeeklySalary());
         System.out.println("Weekly salary of " + emp2.getTitle() + " " + emp2.getFirstName() + " is $" + emp2.calculateWeeklySalary());
         System.out.println("Weekly salary of " + emp3.getTitle() + " " + emp3.getFirstName() + " is $" + emp3.calculateWeeklySalary());
         System.out.println("Weekly salary of " + emp4.getTitle() + " " + emp4.getFirstName() + " is $" + emp4.calculateWeeklySalary());
         System.out.println("Weekly salary of " + emp5.getTitle() + " " + emp5.getFirstName() + " is $" + emp5.calculateWeeklySalary());
         System.out.println("\n");
+        System.out.println("\n******************************************************************");
+        System.out.println("\nTask 3: string representations using the toString method.\n");
         System.out.println(emp1);
         System.out.println(emp2);
         System.out.println(emp3);
         System.out.println(emp4);
         System.out.println(emp5);
-
         System.out.println("\n");
-
+        System.out.println("\n******************************************************************");
+        System.out.println("\nTask 4: Comparing the Employee objects using the equals method.\n");
         System.out.println("emp1 equals emp2? " + emp1.equals(emp2));
         System.out.println("emp2 equals emp3? " + emp2.equals(emp3));
         System.out.println("emp3 equals emp4? " + emp3.equals(emp4));
         System.out.println("emp4 equals emp5? " + emp4.equals(emp5));
         System.out.println("emp5 equals emp1? " + emp5.equals(emp1));
+        System.out.println("\n");
+
+        System.out.println("\n******************************************************************");
+        System.out.println("******************************************************************");
+        System.out.println("******************************************************************");
 
     }
 }
